aed4c143a7fda784674877d1a2b2f4750cbf876f
Eduardo da Silva <edasilva@bol.com>
2018-06-18 23:04:42 +0200

    Updated README with description of the main elements of the WordCount example; extended the WordCountTest with a new test for a DoFn.

--- README.md
+++ README.md
@@ -1,15 +1,37 @@
 # Apache Beam Word Count / Hello World (with subtitles)
 
-This project contains notes on small experiments done with Apache Beam word count example (based on https://beam.apache.org/get-started/wordcount-example/).
+This project contains my notes on small experiments done with Apache Beam word count examples (based on https://beam.apache.org/get-started/wordcount-example/).
 
 ## 1) Minimal Word Count
 [src/main/java/org/apache/beam/examples/MinimalWordCount.java](./src/main/java/org/apache/beam/examples/MinimalWordCount.java)
 
 - Defines simple processing pipeline, explaining the basic elements of an Apache Beam processing pipeline
-- Interesting elements:
+- Summary:
 
   - `PipelineOptions` object: defines the options that are to be used when building an Apache Beam processing pipeline. These can be passed as command-line arguments or obtained from properties files, etc.;
   - `Pipeline` object: this is the actual pipeline object where we can apply different `transform` operations;
   - `PCollection`: collection used to hold the input/output data of `transform` operations;
-  - transform operations: these are the "operations" to be performed on the data (stored as `PCollection`). There are different types of "transforms", example: `Count`; `Filter`; and many other predefined transforms. However, we can also write in-line anonymous functions and execute them in a `ParDo` (Parallel Processing, which is similar to the “Map” phase of a Map/Shuffle/Reduce-style algorithm).
+  - transform operations: these are the "operations" to be performed on the data (stored as `PCollection`). There are different types of "transforms", example: `Count`; `Filter`; and many other predefined transforms. However, we can also write in-line anonymous functions (or have dedicated functions - all subclasses of `DoFn`) and execute them in a `ParDo` (Parallel Processing, which is similar to the “Map” phase of a Map/Shuffle/Reduce-style algorithm).
   - `.apply(<tranform>)`: the `.apply` method enables to define the chain of "transform" elements of the pipeline.
+
+## 2) Word Count
+[src/main/java/org/apache/beam/examples/WordCount.java](./src/main/java/org/apache/beam/examples/WordCount.java)
+
+- Adds several interesting constructs, practices and patterns to write Beam data processing pipelines.
+- Summary:
+
+  - `ParDo` running explicitly defined "functions"/transforms (as `DoFn`): ParDo can execute anonymous functions defined inline. However, it is recommended to define these functions explicitly. The advantages of this are:
+
+    - 1) functions become available for unit testing (testability);
+    - 2) functions are be reused on multiple ParDo (reuse);
+    - 3) makes code of ParDo and pipelines much easier to read (readability).
+
+  - Composite Transforms (`PTransform`): some transforms can "encapsule" multiple smaller transforms/operations. We can either place all those small transforms on the pipeline definition, or instead create a new "composite transform", which basically includes the multiple small transforms. This can be defined using (extending) the `PTransform` class. The advantages of this are:
+
+    - 1) encapsulation of complex behaviors into composite transforms;
+    - 2) reuse of basic transforms across different steps of the pipeline.
+
+  - Unit tests:
+
+    - Beam applications (as many data-driven applications/jobs) may be difficult to unit test (on local machine). Nevertheless, and especially if the Beam pipeline code is arranged in a modular manner (with the patterns presented on the previous points), we can create unit tests per "transformation". These unit tests enable us to be more confident on the code we are building, and they can be run very quickly. All this without having to go into long-running tests using external resources - these are also good to have, but can be made on a later stage. Beam proives
+    - `TestPipeline`, which creates a Pipeline to be used on tests, where we can "apply" the "transforms" we are interested on testing. Example [src/test/java/org/apache/beam/examples/WordCountTest.java](src/test/java/org/apache/beam/examples/WordCountTest.java)
--- src/test/java/org/apache/beam/examples/WordCountTest.java
+++ src/test/java/org/apache/beam/examples/WordCountTest.java
@@ -17,27 +17,27 @@
  */
 package org.apache.beam.examples;
 
-import java.util.Arrays;
-import java.util.List;
 import org.apache.beam.examples.WordCount.CountWords;
-import org.apache.beam.examples.WordCount.ExtractWordsFn;
 import org.apache.beam.examples.WordCount.FormatAsTextFn;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.testing.PAssert;
 import org.apache.beam.sdk.testing.TestPipeline;
 import org.apache.beam.sdk.testing.ValidatesRunner;
 import org.apache.beam.sdk.transforms.Create;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.DoFnTester;
 import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.values.PCollection;
-import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests of WordCount.
@@ -45,41 +45,81 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class WordCountTest {
 
-  /** Example test that tests a specific {@link DoFn}. */
-  @Test
-  public void testExtractWordsFn() throws Exception {
-    DoFnTester<String, String> extractWordsFn =
-        DoFnTester.of(new ExtractWordsFn());
+    // Set up Test data (input and output/expected)
+    private static final String[] WORDS_ARRAY = new String[]{
+            "hi there", "hi", "hi sue bob",
+            "hi sue", "", "bob hi"};
+    private static final String[] WORDS_USED = new String[]{
+            "hi", "there", "sue", "bob"};
+    private static final List<String> WORDS_USED_LIST = Arrays.asList(WORDS_USED);
+    private static final List<String> WORDS = Arrays.asList(WORDS_ARRAY);
+    private static final String[] COUNTS_ARRAY = new String[]{
+            "hi: 5", "there: 1", "sue: 2", "bob: 2"};
 
-    Assert.assertThat(extractWordsFn.processBundle(" some  input  words "),
-                      CoreMatchers.hasItems("some", "input", "words"));
-    Assert.assertThat(extractWordsFn.processBundle(" "), CoreMatchers.hasItems());
-    Assert.assertThat(extractWordsFn.processBundle(" some ", " input", " words"),
-                      CoreMatchers.hasItems("some", "input", "words"));
-  }
+    // Creates a Test Pipeline (can define different PipelineOptions.
+    // By using @Rule, we get the TestPipeline "refreshed" between each unit test (@Test)
+    @Rule
+    public TestPipeline p = TestPipeline.create();
 
-  static final String[] WORDS_ARRAY = new String[] {
-    "hi there", "hi", "hi sue bob",
-    "hi sue", "", "bob hi"};
+    /**
+     * Example test that tests a PTransform by using an in-memory input and inspecting the output.
+     */
+    @Test
+    @Category(ValidatesRunner.class)
+    public void testCountWords() {
+        // define input PCollection in the testing pipeline
+        PCollection<String> input = p.apply(Create.of(WORDS).withCoder(StringUtf8Coder.of()));
+        // apply the CountWords Composite Transform to the "input" PCollection
+        PCollection<String> output = input.apply(new CountWords())
+                .apply(MapElements.via(new FormatAsTextFn()));
+        // asserts that the produced "output" PCollection contains the expected output
+        PAssert.that(output).containsInAnyOrder(COUNTS_ARRAY);
+        // blocks and waits for the pipeline to finish
+        p.run().waitUntilFinish();
+    }
 
-  static final List<String> WORDS = Arrays.asList(WORDS_ARRAY);
+    /**
+     * Test FormatAsTexFn.
+     */
+    @Test
+    @Category(ValidatesRunner.class)
+    public void testExtractWordsFn() {
+        // define input PCollection in the testing pipeline
+        PCollection<String> input = p.apply(Create.of(WORDS).withCoder(StringUtf8Coder.of()));
+        // Convert words of text into individual words.
+        PCollection<String> words = input.apply(
+                ParDo.of(new WordCount.ExtractWordsFn()));
+        // asserts that the tokenized words contain the expected output words
+        PAssert.that(words).satisfies((SerializableFunction<Iterable<String>, Void>) inIt -> {
+            // checks if the WORDS_USED contains all the words extracted
+            inIt.forEach(in ->
+                    assertTrue(WORDS_USED_LIST.contains(in))
+            );
+            return null;
+        });
+        // alterantively: define the SerializableFunction as a separate function (useful if used in multiple places)
+        PAssert.that(words).satisfies(new VerifyWords(WORDS_USED_LIST));
+        // blocks and waits for the pipeline to finish
+        p.run().waitUntilFinish();
+    }
 
-  static final String[] COUNTS_ARRAY = new String[] {
-      "hi: 5", "there: 1", "sue: 2", "bob: 2"};
+    /**
+     * Auxiliary class to verify if words exist
+     */
+    private static class VerifyWords implements SerializableFunction<Iterable<String>, Void> {
+        private final List<String> originalWords;
 
-  @Rule
-  public TestPipeline p = TestPipeline.create();
+        private VerifyWords(List<String> originalWorlds) {
+            this.originalWords = originalWorlds;
+        }
 
-  /** Example test that tests a PTransform by using an in-memory input and inspecting the output. */
-  @Test
-  @Category(ValidatesRunner.class)
-  public void testCountWords() throws Exception {
-    PCollection<String> input = p.apply(Create.of(WORDS).withCoder(StringUtf8Coder.of()));
+        @Override
+        public Void apply(Iterable<String> actualIter) {
+            System.out.println("TEST: originalWords = " + originalWords);
+            for (String s : actualIter)
+                assertTrue(originalWords.contains(s));
+            return null;
+        }
+    }
 
-    PCollection<String> output = input.apply(new CountWords())
-      .apply(MapElements.via(new FormatAsTextFn()));
-
-    PAssert.that(output).containsInAnyOrder(COUNTS_ARRAY);
-    p.run().waitUntilFinish();
-  }
 }
